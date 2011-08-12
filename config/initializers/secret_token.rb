9d09e83a459f2a5d00fcccb6c6ec1f1356adf62c
Robin <robinroestenburg@gmail.com>
2011-08-12 20:00:37 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mtg::Application.config.secret_token = '8d7010ffa56131c3e9e86148be6efca4b461cbbb35488110f483bcba55306bd7bae28cb5bdf573186096e8b33894c3597d635755e85562bf45fe6b4ed815478d'
